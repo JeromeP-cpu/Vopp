ded699fd458cc3014afa3ae1cae192a64acf8f55
JeromeP-cpu <jrpieh0408@gmail.com>
2025-12-02 15:06:59 +0800

    create a lib vectorOp & imolement vAdd (tested)

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"vectorOp_h"
+
+int main(){
+    int A[3]={1,2,3};
+    int B[3]={3,6,8};
+
+    int c[3];
+    c=vADD[A,B,3];
+    int i=0;
+    for(i=0;i<3;i++)
+    {
+        printf("%d",c[i]);
+    }
+    return 0;
+}
---
+++ vectorOp.h
@@ -0,0 +1,9 @@
+#ifndef VECTOROP_H
+
+#define VECTOROP_H
+
+int* vADD(int *A,int *B,int size);
+int* vSUB(int *A,int *B,int size);
+int* vDOT(int *A,int *B,int size);
+
+#endif
