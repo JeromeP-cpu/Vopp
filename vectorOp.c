d0440bb15692d75e793c2936c33553e477fc1bf5
JeromeP-cpu <jrpieh0408@gmail.com>
2025-12-02 15:48:31 +0800

    subtraction

--- vectorOp.c
+++ vectorOp.c
@@ -1,6 +1,6 @@
 #include "vectorOp.h"
 #include<stdio.h>
-int* vADD(int *A,int *B,int size);
+int* vADD(int *A,int *B,int size)
 {
     int *c=malloc(size*sizeof(int));
 
@@ -9,5 +9,16 @@ int* vADD(int *A,int *B,int size);
     {
         c[i]=A[i]+B[i];
     }
-    
+    return c;
+}
+int* vSUB(int *A,int *B,int size)
+{
+    int *c=malloc(size*sizeof(int));
+
+    int i=0;
+    for(i=0;i<size;i++)
+    {
+        c[i]=A[i]-B[i];
+    }
+    return c;
 }
